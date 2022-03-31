af7c9ef1bba522059c59cf477282f7e3715d33bd
EgorVoron <ekvoron01@gmail.com>
2022-03-31 15:36:26 +0300

    works

--- gui_class.py
+++ gui_class.py
@@ -35,100 +35,110 @@ class Window(QMainWindow):
         self.time_problem = False
         self.same_time = True
 
-        self.building_label = QtWidgets.QLabel(self)
-        self.building_label.setText("Корпус:")
-        self.building_label.adjustSize()
-        self.building_label.move(30, 107)
+        building_label = QtWidgets.QLabel(self)
+        building_label.setText("Корпус:")
+        building_label.adjustSize()
+        building_label.move(30, 107)
 
-        self.combo = QtWidgets.QComboBox(self)
-        self.combo.addItem("Любой")
-        self.combo.addItem("Главный (гк)")
-        self.combo.addItem("Лабораторный (лк)")
-        self.combo.addItem("Новый (нк)")
-        self.combo.move(100, 100)
+        combo = QtWidgets.QComboBox(self)
+        combo.addItem("Любой")
+        combo.addItem("Главный (гк)")
+        combo.addItem("Лабораторный (лк)")
+        combo.addItem("Новый (нк)")
+        combo.move(100, 100)
 
-        self.number_label = QtWidgets.QLabel(self)
-        self.number_label.setText("Кол-во людей:")
-        self.number_label.adjustSize()
-        self.number_label.move(250, 107)
+        number_label = QtWidgets.QLabel(self)
+        number_label.setText("Кол-во людей:")
+        number_label.adjustSize()
+        number_label.move(250, 107)
 
-        self.line = QtWidgets.QLineEdit(self)
-        self.line.move(350, 100)
-        self.onlyInt = QIntValidator()
-        self.line.setValidator(self.onlyInt)
+        line = QtWidgets.QLineEdit(self)
+        line.move(350, 100)
+        onlyInt = QIntValidator()
+        line.setValidator(onlyInt)
 
-        self.start_datetime = QDateTimeEdit(self)
-        self.start_datetime.setGeometry(580, 100, 150, 30)
-        self.cur_dt = QDateTime.currentDateTime()
-        self.start_datetime.setDateTime(self.cur_dt)
-        self.start_label = QLabel("Время начала:", self)
-        self.start_label.setGeometry(480, 85, 200, 60)
-        self.start_label.setWordWrap(True)
-        self.start_warning = QLabel("", self)
-        self.start_warning.setGeometry(580, 140, 200, 60)
-        self.start_warning.setWordWrap(True)
-        self.start_datetime.dateTimeChanged.connect(lambda: self.dt_method(self.start_datetime))
+        start_datetime = QDateTimeEdit(self)
+        start_datetime.setGeometry(580, 100, 150, 30)
+        cur_dt = QDateTime.currentDateTime()
+        start_datetime.setDateTime(cur_dt)
+        start_label = QLabel("Время начала:", self)
+        start_label.setGeometry(480, 85, 200, 60)
+        start_label.setWordWrap(True)
+        start_warning = QLabel("", self)
+        start_warning.setGeometry(580, 140, 200, 60)
+        start_warning.setWordWrap(True)
+        start_datetime.dateTimeChanged.connect(lambda: dt_method(start_datetime))
 
-        self.end_datetime = QDateTimeEdit(self)
-        self.end_datetime.setDateTime(self.cur_dt)
-        self.end_datetime.setGeometry(880, 100, 150, 30)
-        self.end_label = QLabel("Время конца:", self)
-        self.end_label.setGeometry(780, 85, 200, 60)
-        self.end_label.setWordWrap(True)
-        self.end_warning = QLabel("", self)
-        self.end_warning.setGeometry(880, 140, 200, 60)
-        self.end_warning.setWordWrap(True)
-        self.end_datetime.dateTimeChanged.connect(lambda: self.dt_method2(self.end_datetime))
+        def dt_method(start_datetime, win=self):
+            if start_datetime.dateTime().toPyDateTime() < datetime.datetime.now():
+                start_warning.setText('Время начала не может быть раньше текущего!')
+                win.time_problem = True
+            else:
+                win.time_problem = False
+                start_warning.setText('')
 
-        button = QtWidgets.QPushButton(self)
-        button.clicked.connect(self.run)
-        button.setText("Найти подходящие комнаты")
-        button.setGeometry(100, 250, 200, 50)
+        end_datetime = QDateTimeEdit(self)
+        end_datetime.setDateTime(cur_dt)
+        end_datetime.setGeometry(880, 100, 150, 30)
+        end_label = QLabel("Время конца:", self)
+        end_label.setGeometry(780, 85, 200, 60)
+        end_label.setWordWrap(True)
+        end_warning = QLabel("", self)
+        end_warning.setGeometry(880, 140, 200, 60)
+        end_warning.setWordWrap(True)
+        end_datetime.dateTimeChanged.connect(lambda: dt_method2(end_datetime))
+
+        def dt_method2(end_datetime, win=self):
+            if end_datetime.dateTime().toPyDateTime() < start_datetime.dateTime().toPyDateTime():
+                end_warning.setText('Время конца не может быть меньше времени начала!')
+                win.time_problem = True
+            elif end_datetime.dateTime().toPyDateTime() - start_datetime.dateTime().toPyDateTime() > datetime.timedelta(
+                    hours=5):
+                end_warning.setText('Нельзя бронировать больше, чем на 5 часов!')
+                win.time_problem = True
+            elif end_datetime.dateTime().toPyDateTime() == start_datetime.dateTime().toPyDateTime():
+                end_warning.setText('Время начала и конца не может совпадать!')
+                win.same_time = True
+            else:
+                win.same_time = False
+                win.time_problem = False
+                end_warning.setText('')
+            print(end_datetime.dateTime().toPyDateTime())
+            print(start_datetime.dateTime().toPyDateTime())
 
         error_label = QLabel("", self)
         error_label.setGeometry(100, 200, 200, 15)
 
-    def dt_method(self, start_datetime):
-        if start_datetime.dateTime().toPyDateTime() < datetime.datetime.now():
-            self.start_warning.setText('Время начала не может быть раньше текущего!')
-            self.time_problem = True
-        else:
-            self.time_problem = False
-            self.start_warning.setText('')
+        def run(win=self):
+            if not line.text():
+                error_label.setText('Введите число людей!')
+                print('Введите число людей!')
+            elif win.time_problem:
+                error_label.setText('Исправьте время')
+                print('Исправьте время')
+            elif win.same_time:
+                error_label.setText('Время начала и конца совпадает!')
+                print('Иремя начала и конц')
+            else:
+                print('h')
+                error_label.setText('')
+                get_recommendations(start_time=qt_time_to_unix(start_datetime),
+                                    end_time=qt_time_to_unix(end_datetime),
+                                    people_num=int(line.text()),
+                                    building=str(combo.currentText()))
+                self.show_results()
+            res_lab = QLabel("ПИЗДА", win)
+            res_lab.setGeometry(100, 200, 200, 15)
 
-    def dt_method2(self, end_datetime):
-        if end_datetime.dateTime().toPyDateTime() < self.start_datetime.dateTime().toPyDateTime():
-            self.end_warning.setText('Время конца не может быть меньше времени начала!')
-            self.time_problem = True
-        elif end_datetime.dateTime().toPyDateTime() - self.start_datetime.dateTime().toPyDateTime() > datetime.timedelta(
-                hours=5):
-            self.end_warning.setText('Нельзя бронировать больше, чем на 5 часов!')
-            self.time_problem = True
-        elif end_datetime.dateTime().toPyDateTime() == self.start_datetime.dateTime().toPyDateTime():
-            self.end_warning.setText('Время начала и конца не может совпадать!')
-            self.same_time = True
-        else:
-            self.same_time = False
-            self.time_problem = False
-            self.end_warning.setText('')
+        button = QtWidgets.QPushButton(self)
+        button.clicked.connect(lambda: run())
+        button.setText("Найти подходящие комнаты")
+        button.setGeometry(100, 250, 200, 50)
 
-    def run(self):
-        print(self.line.text())
-        if not self.line.text():
-            self.error_label.setText('Введите число людей!')
-            print('Введите число людей!')
-        elif self.time_problem:
-            self.error_label.setText('Исправьте время')
-            print('Исправьте время')
-        elif self.same_time:
-            self.error_label.setText('Время начала и конца совпадает!')
-            print('Иремя начала и конц')
-        else:
-            self.error_label.setText('')
-            get_recommendations(start_time=qt_time_to_unix(self.start_datetime),
-                                end_time=qt_time_to_unix(self.end_datetime),
-                                people_num=int(self.line.text()),
-                                building=str(self.combo.currentText()))
+    def show_results(self):
+        button = QtWidgets.QPushButton(self)
+        button.setText("Найти adsasda комнаты")
+        button.setGeometry(100, 300, 200, 50)
 
 
 App = QApplication(sys.argv)
